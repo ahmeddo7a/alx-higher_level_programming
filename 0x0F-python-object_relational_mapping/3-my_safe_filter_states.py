20aa46207d43e5848786afcd2a07812a0dd06801
ahmeddo7a <ahmedholdo@gmail.com>
2023-08-25 00:52:41 -0700

    fix1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """
-Script that takes argument and display values 
-in states table where name mathches argument
-but safe from sql injection
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
-
 import MySQLdb
 from sys import argv
 
-# The code should not be excuted when imported
+# The code should not be executed when imported
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", argv[4])
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for i in rows:
         print(i)
+    # Clean up process
     cur.close()
     db.close()
